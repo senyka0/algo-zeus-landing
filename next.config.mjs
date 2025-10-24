debb3263c4ba474bbe96b24de86d1e90c09f8aba
senyka0 <arsk293@gmail.com>
2025-10-24 20:26:00 +0300

    added gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    },
     basePath: '/hedge-fund-landing',
     assetPrefix: '/hedge-fund-landing/',
 };
